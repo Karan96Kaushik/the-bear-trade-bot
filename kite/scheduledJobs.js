4e027a0e2c4b58544d1c5e5cf8fe3219ac8472d4
Karan96Kaushik <karankshk96@gmail.com>
2024-10-22 08:56:23 +0100

    sch update

--- kite/scheduledJobs.js
+++ kite/scheduledJobs.js
@@ -10,7 +10,7 @@ const { connectToDatabase } = require('../modules/db');
 
 const sellSch = process.env.NODE_ENV === 'production' ? 
                     // '16 5 * * 1-5' : 
-                    '56 7 * * 1-5' : 
+                    '57 7 * * 1-5' : 
                     '17 16 * * 1-5'
 
 const buySch = process.env.NODE_ENV === 'production' ? 
