e34e9db481ca5a4d74af1c84e715433baade190c
Karan96Kaushik <karankshk96@gmail.com>
2024-10-28 04:38:00 +0000

    minor fix

--- kite/scheduledJobs.js
+++ kite/scheduledJobs.js
@@ -295,7 +295,7 @@ const scheduleMISJobs = () => {
     // sendMessageToChannel('⏰ Special MIS Scheduled - ', getDateStringIND(specialJob.nextInvocation()));
 
     // const zaireJob = schedule.scheduleJob('11 4 * * *', () => {
-    const zaireJob = schedule.scheduleJob('1,16 4 * * 1-5', () => {
+    const zaireJob = schedule.scheduleJob('1,19 4 * * 1-5', () => {
         sendMessageToChannel('⏰ Zaire MIS Scheduled - ', getDateStringIND(zaireJob.nextInvocation()));
         setupZaireOrders();
     });
