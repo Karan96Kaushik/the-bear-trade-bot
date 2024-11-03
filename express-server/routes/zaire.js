f757d3a2bec990ae5470cd08d5a14fed51ec0d20
Karan96Kaushik <karankshk96@gmail.com>
2024-11-03 16:03:05 +0530

    added zaire route support for other intervals

--- express-server/routes/zaire.js
+++ express-server/routes/zaire.js
@@ -7,19 +7,24 @@ const { scanZaireStocks } = require('../../analytics');
 router.get('/selected-stocks', async (req, res) => {
     // Get date from query or use current date
     let date = req.query.date ? new Date(req.query.date) : new Date();
+    let interval = req.query.interval ? req.query.interval : '5m';
+    const intervalMins = parseInt(interval.split('m')[0]);
     
     // Round to nearest 15 minutes
-    date.setMinutes(Math.round(date.getMinutes() / 15) * 15);
+    date.setMinutes(Math.round(date.getMinutes() / intervalMins) * intervalMins);
     date.setSeconds(10); // Add 10 seconds
     
-    if (date.getUTCHours() < 4) {
+    if (date.getUTCHours() < 4 && intervalMins >= 15) {
         date.setUTCHours(4,0,10,0);
+    }
+    else if (date.getUTCHours() < 4 && intervalMins < 15) {
+        date.setUTCHours(3,50,10,0);
     }
 
     let niftyList = await readSheetData('Nifty!A1:A200')
     niftyList = niftyList.map(stock => stock[0])
 
-    let selectedStocks = await scanZaireStocks(niftyList, date);
+    let selectedStocks = await scanZaireStocks(niftyList, date, interval);
     selectedStocks = selectedStocks.map(a => ({...a, qty: Math.ceil(200/(a.high - a.low))}))
 
     res.json({stocks: selectedStocks})
