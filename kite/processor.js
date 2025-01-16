d9ff7049f72a547ce6eb9c5b87b0d14383df6bbc
Karan96Kaushik <karankshk96@gmail.com>
2025-01-16 12:52:49 +0000

    SL price updated to padding

--- kite/processor.js
+++ kite/processor.js
@@ -364,7 +364,7 @@ async function createZaireOrders(stock, tag='zaire') {
             // Trigger price is 0.05% above high
             triggerPrice = stock.high + triggerPadding;
             // Stop loss is low
-            stopLossPrice = stock.low - 1;
+            stopLossPrice = stock.low - triggerPadding;
             // Target price is double the difference between high and low plus trigger price
             targetPrice = ((stock.high - stock.low) * 2) + stock.high  // triggerPrice;
 
@@ -407,7 +407,7 @@ async function createZaireOrders(stock, tag='zaire') {
             // Trigger price is 0.05% below low 
             triggerPrice = stock.low - triggerPadding;
             // Stop loss is high
-            stopLossPrice = stock.high + 1;
+            stopLossPrice = stock.high + triggerPadding;
             // Target price is double the difference between trigger price and low
             targetPrice = (stock.low - (stock.high - stock.low)* 2);
 
