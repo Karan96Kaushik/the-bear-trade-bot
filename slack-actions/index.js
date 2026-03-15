8466cb8fc8934ce3b4cc073467a1d76253ec922c
Karan96Kaushik <karankshk96@gmail.com>
2026-03-15 23:22:16 +0000

    minor

--- slack-actions/index.js
+++ slack-actions/index.js
@@ -345,7 +345,8 @@ async function logMessageToCSV(channel_name, message) {
 			? message.map(s => typeof(s) == 'object' ? JSON.stringify(s) : String(s)).join(' ')
 			: String(message);
 		
-		const escapedMessage = `"${messageText.replace(/"/g, '""')}"`;
+		const fullMessage = `[${channel_name}] ${messageText}`;
+		const escapedMessage = `"${fullMessage.replace(/"/g, '""')}"`;
 		const escapedChannel = `"${channel_name.replace(/"/g, '""')}"`;
 		const csvLine = `${timestamp},${escapedChannel},${escapedMessage}\n`;
 		
