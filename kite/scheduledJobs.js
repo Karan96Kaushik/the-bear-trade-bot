60faeb22819adc10573a06a74b23fe7f65806cc0
Karan96Kaushik <karankshk96@gmail.com>
2026-03-19 13:01:40 +0000

    updated baxter sch to start at 9:20

--- kite/scheduledJobs.js
+++ kite/scheduledJobs.js
@@ -1034,7 +1034,7 @@ const scheduleMISJobs = () => {
             };
             // First run at 3:55 AM, then every 15 minutes during trading hours
             // Cancels all pending orders before creating new ones
-            const baxterJob = schedule.scheduleJob('45 55 3 * * 1-5', baxterJobCB);
+            const baxterJob = schedule.scheduleJob('45 50 3 * * 1-5', baxterJobCB);
             const baxterJob_2 = schedule.scheduleJob('45 */15 4,5,6,7,8 * * 1-5', baxterJobCB);
             sendMessageToChannel('⏰ Baxter Setup Scheduled - ', getDateStringIND(getEarliestTime(baxterJob, baxterJob_2)));
         }
