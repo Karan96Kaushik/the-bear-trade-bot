16a1e2dc109e15a7d11a54169f1a236ea8f7cfce
Karan96Kaushik <karankshk96@gmail.com>
2026-03-28 12:42:27 +0000

    timer for baxter

--- kite/scheduledJobs.js
+++ kite/scheduledJobs.js
@@ -1034,9 +1034,9 @@ const scheduleMISJobs = () => {
             };
             // First run at 3:55 AM, then every 15 minutes during trading hours
             // Cancels all pending orders before creating new ones
-            const baxterJob = schedule.scheduleJob('45 50,55 3 * * 1-5', baxterJobCB);
-            const baxterJob_2 = schedule.scheduleJob('45 */5 4,5,6,7,8 * * 1-5', baxterJobCB);
-            const baxterJob_3 = schedule.scheduleJob('45 0,5,10,15,20,25,30 * * 1-5', baxterJobCB);
+            const baxterJob = schedule.scheduleJob('35 50,55 3 * * 1-5', baxterJobCB);
+            const baxterJob_2 = schedule.scheduleJob('35 */5 4,5,6,7,8 * * 1-5', baxterJobCB);
+            const baxterJob_3 = schedule.scheduleJob('35 0-30/5 9 * * 1-5', baxterJobCB);
             sendMessageToChannel('⏰ Baxter Setup Scheduled - ', getDateStringIND(getEarliestTime(baxterJob, baxterJob_2, baxterJob_3)));
         }
 
